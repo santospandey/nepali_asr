8b16ed5695402d8c0cd74789cc0c6106ea8e05d3
Santosh Pandey <santospandey333@gmail.com>
2024-09-22 20:40:50 +0545

    added

--- training.py
+++ training.py
@@ -192,7 +192,7 @@ if __name__ == "__main__":
     optimizer = tf.keras.optimizers.Adam()
 
     print("Loading data.....")
-    train_wavs, train_texts = load_data("download/wavs", "download/transcripts/utt_spk_text.tsv", 100)
+    train_wavs, train_texts = load_data("download/wavs", "download/transcripts/utt_spk_text.tsv", 10)
     print("Data loaded \u2705 \u2705 \u2705 \u2705\n")
 
     print("Cleaning the audio files.....")
